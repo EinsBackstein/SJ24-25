e230244d72adf2c9feb7ef4137dccdc621f18dd9
Julian <juli.nott@gmx.at>
2024-09-23 14:56:54 +0200

    fixed location of .py

---
+++ sew01_python_basics/hello_world.py
@@ -0,0 +1,11 @@
+# convert the string to an int
+height = input("Please enter the height: ")
+height = float(height)
+
+# short way
+width = float(input("Please enter the width: "))
+
+area = height * width
+
+# convert the int result to a string again
+print("area = " + str(area))
---
+++ sew02_guessing_game/guessing_game.py
@@ -0,0 +1,8 @@
+# import the function 'randint' from the standard module 'random'
+from random import randint
+
+# the maximum number (should be asked from the user)
+max_number = 10
+
+# the function randint generates a random number from 1 to max_num
+random_number = randint(1, max_number)
