9621686104f3541ac9b3d2469373c5aff10e57cd
Julian <juli.nott@gmx.at>
2024-09-30 15:56:23 +0200

    11

--- sew03_marks_manager/marks_manager.py
+++ sew03_marks_manager/marks_manager.py
@@ -1,10 +1,28 @@
 # Initial User Information
 print("\n1 - Enter a Subject with mark \n2 - Remove a Subject \n3 - Show all subjects \n0 - Exit\n")
 
-user_input = input("Please input your choice: ")
+grades = {}
 
+class Subject:
+    def __init__(self, name):
+        self.name = name
+        self.mark = {1,2,3}
+    def add_mark(self, mark):
+        self.mark.add(mark)
+    def __str__(self):
+        return f"Subject:\t {self.name} \nMark/s:\t\t {self.mark}\n"
+
+user_input = input("Please input your choice: ")
 def add_subject():
-    print("Adding subject...")
+    input_subject = input("Please input the subject: ")
+    input_mark = input("Please input the mark: ")
+
+    if input_subject not in grades:
+        subject = Subject(input_subject)
+        grades[subject.name] = subject
+        subject.add_mark(input_mark)
+    else:
+        grades[input_subject].add_mark(input_mark)
 
 def remove_subject():
     print("Removing subject...")
@@ -32,5 +50,3 @@ def check_input(user_input):
         check_input(user_input)
 
 check_input(user_input)
-
-#test
